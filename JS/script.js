abe593d6966bcab9ebdfd3c72c04a88f98036d76
Viviana <40200339@esmad.ipp.pt>
2026-01-20 19:23:03 +0000

    added all the js logic of the project cards and the array with the projects. Array structure complete, contents might vary depending on which projects I want to showcase

---
+++ JS/script.js
@@ -0,0 +1,182 @@
+// Liberdade Cravada (featured)
+
+// ALVID-19 (award + teamwork)
+
+// React Calculator
+
+// Liberty Breakout
+
+// Breakout Game 
+
+// Asteroids Browser-based 3D reinterpretation of the classic Asteroids game built with Three.js.
+
+let projects = [
+    { /** LIBERDADE CRAVADA */ //feito
+        logo: "./images/LC_logo.png",
+        logo_alt: 'logo of the project "Liberdade Cravada"',
+        title: "Liberdade Cravada",
+        img: "./images/LC_screenshot.png",
+        alt: 'first section of the main page of the project "Liberdade Cravada"',
+        description: "Interactive web experience exploring the Portuguese Revolution through gamified interaction and data visualisation.",
+        tech: [
+            {name: "JavaScript", img:"https://imgs.search.brave.com/s95Fx2_gXkyBRwh25xOsVWeoBWrlHfiF5ANp-8xgIoM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzI3LzIvamF2YXNj/cmlwdC1qcy1sb2dv/LXBuZ19zZWVrbG9n/by0yNzM1NTcucG5n", alt: "logo of javascript"},
+            {name: "D3.js", img:"https://imgs.search.brave.com/IwWmXgdExAsOEhkFvEcM0xl3Uu5qlZpvf4C3LJB7t5E/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9kM2pzLnN2Zw", alt:"logo of d3.js"},
+            {name: "p5.js", img:"./images/p5.png", alt:"logo of p5.js"},
+            {name: "ml5.js", img:"https://imgs.search.brave.com/0v6XtuuwtG6u_U_DTZuCpDrs_860I4dzMsCw9NISdT8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hcmNo/aXZlLm1sNWpzLm9y/Zy9zdGF0aWMvMTU1/MmFiNzFlMTM0ZDNm/NmFhZWQwYzM5ZmJj/MGI4M2MvNGJhZDIv/bG9nby1wdXJwbGUt/Y2lyY2xlLnBuZw", alt: "logo of ml5.js"},
+            {name: "HTML5", img:"https://imgs.search.brave.com/-A7DMn2sD4NJtodxwkO-SQM6qd5-pMzItJulgw-SvIQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9odG1sLTEuc3Zn", alt: "logo of HTML"},
+            {name: "CSS3", img:"https://imgs.search.brave.com/RgVw7TR4U4riBvB4_Aie0ngUM5orFW9NSnu-0lrFJkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9jc3MtMy5zdmc", alt: "logo of CSS"},
+        ],
+        demo: "https://vivi4na02l.github.io/Liberdade-Cravada/",
+        github: "https://github.com/Vivi4na02l/Liberdade-Cravada"
+    },
+    { /** ALVID-19 */ //feito
+        logo: "./images/alvid_logo.png",
+        logo_alt: 'logo of the project "Alvid-19"',
+        title: "Alvid-19",
+        img: "./images/alvid-19_screenshot.png",
+        alt: 'first section of the main page of the project "Alvid-19"',
+        description: "Award-winning gamified educational web platform featuring user authentication, roles and interactive games.",
+        tech: [
+            {name: "JavaScript", img:"https://imgs.search.brave.com/s95Fx2_gXkyBRwh25xOsVWeoBWrlHfiF5ANp-8xgIoM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzI3LzIvamF2YXNj/cmlwdC1qcy1sb2dv/LXBuZ19zZWVrbG9n/by0yNzM1NTcucG5n", alt: "logo of javascript"},
+            {name: "HTML5", img:"https://imgs.search.brave.com/-A7DMn2sD4NJtodxwkO-SQM6qd5-pMzItJulgw-SvIQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9odG1sLTEuc3Zn", alt: "logo of HTML"},
+            {name: "CSS3", img:"https://imgs.search.brave.com/RgVw7TR4U4riBvB4_Aie0ngUM5orFW9NSnu-0lrFJkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9jc3MtMy5zdmc", alt: "logo of CSS"},
+            {name: "MVC", img:false, alt:false},
+        ],
+        demo: "https://vivi4na02l.github.io/Alvid-19/Alvid-19/",
+        github: "https://github.com/Vivi4na02l/Alvid-19"
+    },
+    { /** REACT CALCULATOR */ //feito
+        logo: "./images/alvid_logo.png",
+        logo_alt: 'logo of the project "Alvid-19"',
+        title: "React Calculator",
+        img: "./images/alvid-19_screenshot.png",
+        alt: 'screenshot of the react calculator project',
+        description: "Simple React calculator focused on clean component structure and predictable state management.",
+        tech: [
+            {name: "React", img:"https://imgs.search.brave.com/-wRYfmc8MFTE9dYoFBioZSxfu45QaJyuKfJ3h68gZGM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvbG9nb3MtMy82/MDAvUmVhY3QuanNf/bG9nby01MTIucG5n", alt: "logo of React"},
+            {name: "JavaScript", img:"https://imgs.search.brave.com/s95Fx2_gXkyBRwh25xOsVWeoBWrlHfiF5ANp-8xgIoM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzI3LzIvamF2YXNj/cmlwdC1qcy1sb2dv/LXBuZ19zZWVrbG9n/by0yNzM1NTcucG5n", alt: "logo of javascript"},
+            {name: "CSS3", img:"https://imgs.search.brave.com/RgVw7TR4U4riBvB4_Aie0ngUM5orFW9NSnu-0lrFJkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9jc3MtMy5zdmc", alt: "logo of CSS"},
+        ],
+        demo: "",
+        github: "https://github.com/Vivi4na02l/calculator_react"
+    },
+    { /** LIBERTY BREAKOUT */ //feito
+        logo: "./images/LC_logo.png",
+        logo_alt: 'logo of the project "Liberty Breakout"',
+        title: "Liberty Breakout",
+        img: "../images/LB_screenshot.png",
+        alt: 'screenshot of the main menu of Liberty Breakout',
+        description: "Gamified interactive project exploring the April 25, 1974 revolution through mini-games and alternative input methods.",
+        tech: [
+            {name: "JavaScript", img:"https://imgs.search.brave.com/s95Fx2_gXkyBRwh25xOsVWeoBWrlHfiF5ANp-8xgIoM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzI3LzIvamF2YXNj/cmlwdC1qcy1sb2dv/LXBuZ19zZWVrbG9n/by0yNzM1NTcucG5n", alt: "logo of javascript"},
+            {name: "p5.js", img:"./images/p5.png", alt:"logo of p5.js"},
+            {name: "Arduino", img:"https://imgs.search.brave.com/FW1BpdqbKcF55DbjQeH7VCg6AeaCNKGgs3tebxQ-cXI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/aWNvbnNjb3V0LmNv/bS9pY29uL2ZyZWUv/cG5nLTI1Ni9mcmVl/LWFyZHVpbm8taWNv/bi1zdmctZG93bmxv/YWQtcG5nLTIyNjA3/Mi5wbmc_Zj13ZWJw/Jnc9MTI4", alt: "logo of arduino"},
+            {name: "HTML5", img:"https://imgs.search.brave.com/-A7DMn2sD4NJtodxwkO-SQM6qd5-pMzItJulgw-SvIQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9odG1sLTEuc3Zn", alt: "logo of HTML"},
+            {name: "CSS3", img:"https://imgs.search.brave.com/RgVw7TR4U4riBvB4_Aie0ngUM5orFW9NSnu-0lrFJkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9jc3MtMy5zdmc", alt: "logo of CSS"},
+        ],
+        demo: "https://vivi4na02l.github.io/Liberty-Breakout/P5_to_Arduino/index.html",
+        github: "https://github.com/Vivi4na02l/Liberty-Breakout"
+    },
+    { /** VAPORWAVE BREAKOUT */ //feito
+        logo: "./images/LC_logo.png",
+        logo_alt: 'logo of the project "Vaporwave Breakout"',
+        title: "Vaporwave Breakout",
+        img: "../images/VB_screenshot.png",
+        alt: 'screenshot of the main menu of Vaporwave Breakout',
+        description: "Reinterpretation of the classic Breakout game focused on experimenting with alternative interaction methods.",
+        tech: [
+            {name: "JavaScript", img:"https://imgs.search.brave.com/s95Fx2_gXkyBRwh25xOsVWeoBWrlHfiF5ANp-8xgIoM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzI3LzIvamF2YXNj/cmlwdC1qcy1sb2dv/LXBuZ19zZWVrbG9n/by0yNzM1NTcucG5n", alt: "logo of javascript"},
+            {name: "p5.js", img:"./images/p5.png", alt:"logo of p5.js"},
+            {name: "ml5.js", img:"https://imgs.search.brave.com/0v6XtuuwtG6u_U_DTZuCpDrs_860I4dzMsCw9NISdT8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hcmNo/aXZlLm1sNWpzLm9y/Zy9zdGF0aWMvMTU1/MmFiNzFlMTM0ZDNm/NmFhZWQwYzM5ZmJj/MGI4M2MvNGJhZDIv/bG9nby1wdXJwbGUt/Y2lyY2xlLnBuZw", alt: "logo of ml5.js"},
+            {name: "Arduino", img:"https://imgs.search.brave.com/FW1BpdqbKcF55DbjQeH7VCg6AeaCNKGgs3tebxQ-cXI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/aWNvbnNjb3V0LmNv/bS9pY29uL2ZyZWUv/cG5nLTI1Ni9mcmVl/LWFyZHVpbm8taWNv/bi1zdmctZG93bmxv/YWQtcG5nLTIyNjA3/Mi5wbmc_Zj13ZWJw/Jnc9MTI4", alt: "logo of arduino"},
+            {name: "HTML5", img:"https://imgs.search.brave.com/-A7DMn2sD4NJtodxwkO-SQM6qd5-pMzItJulgw-SvIQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9odG1sLTEuc3Zn", alt: "logo of HTML"},
+            {name: "CSS3", img:"https://imgs.search.brave.com/RgVw7TR4U4riBvB4_Aie0ngUM5orFW9NSnu-0lrFJkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9jc3MtMy5zdmc", alt: "logo of CSS"},
+        ],
+        demo: "https://vivi4na02l.github.io/Breakout-game/P5_to_Arduino/index.html",
+        github: "https://github.com/Vivi4na02l/Breakout-game"
+    },
+    { /** ASTEROIDS */ //feito
+        logo: "./images/A_logo.png",
+        logo_alt: 'logo of the project "Asteroids"',
+        title: "Asteroids",
+        img: "../images/A_screenshot.png",
+        alt: 'screenshot of the part of the gameplay of Asteroids',
+        description: "A 2D browser-based reimagining of the classic Asteroids game with solid boundaries, physics-based collisions, and mouse-aimed shooting mechanics.",
+        tech: [
+            {name: "JavaScript", img:"https://imgs.search.brave.com/s95Fx2_gXkyBRwh25xOsVWeoBWrlHfiF5ANp-8xgIoM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzI3LzIvamF2YXNj/cmlwdC1qcy1sb2dv/LXBuZ19zZWVrbG9n/by0yNzM1NTcucG5n", alt: "logo of javascript"},
+            {name: "Three.js", img:"https://imgs.search.brave.com/POddfkSkDEQORcswQ1JfXWpvf3y9rkeOnwJ-8TOej7s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jYW5h/ZGExLmRpc2NvdXJz/ZS1jZG4uY29tL2Zs/ZXgwMzUvdXBsb2Fk/cy90aHJlZWpzL29y/aWdpbmFsLzJYL2Iv/YmUyZjc1ZjcyNzUx/YzExY2JlMTU5M2M2/OWE5OWE1MjkwMGJm/MTJjYi5zdmc", alt:"logo of three.js"},
+            {name: "HTML5", img:"https://imgs.search.brave.com/-A7DMn2sD4NJtodxwkO-SQM6qd5-pMzItJulgw-SvIQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9odG1sLTEuc3Zn", alt: "logo of HTML"},
+            {name: "CSS3", img:"https://imgs.search.brave.com/RgVw7TR4U4riBvB4_Aie0ngUM5orFW9NSnu-0lrFJkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy9jc3MtMy5zdmc", alt: "logo of CSS"},
+            {name: "Bootstrap", img:"https://imgs.search.brave.com/linDLuCrb1B71Jtl1QP-3vAdm7VNtPqEZWlf2RStp44/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9i/L2IyL0Jvb3RzdHJh/cF9sb2dvLnN2Zw", alt:"logo of Bootstrap"},
+        ],
+        demo: "https://vivi4na02l.github.io/Asteroids/",
+        github: "https://github.com/Vivi4na02l/Asteroids"
+    },
+]
+
+let projectsHtml = ''
+for (const project of projects) {
+    projectsHtml += `
+        <div>
+            <article class="project">
+                <!-- header -->
+                <header>
+                    <div>
+                        <img src="${project.logo}"
+                            alt='${project.logo_alt}'
+                            width="5%">
+                        <h3>${project.title}</h3>
+                    </div>
+                    
+                    <div>
+                        <span class="headerBtn minimizeBtn">&#8211;</span>
+                        <span class="headerBtn fullscreenBtn">&#9723;</span>
+                        <span class="closeBtn">✕</span>
+                    </div>
+                </header>
+
+                <!-- body -->
+                <div class="body">
+                    <img src="${project.img}"
+                        alt='${project.alt}'
+                        width="100%"
+                        class="projectImg">
+
+                    <p class="description">${project.description}</p>
+
+                    <span class="projectTech">`
+
+    for (const projectTech of project.tech) {
+        if (!projectTech.img) {
+           projectsHtml += `
+                <figure>
+                    <figcaption>${projectTech.name}</figcaption>
+                </figure>
+            ` 
+        } else {
+            projectsHtml += `
+                <figure>
+                    <img src="${projectTech.img}"
+                        alt="${projectTech.alt} ">
+                    <figcaption>${projectTech.name}</figcaption>
+                </figure>
+            `
+        }
+    }
+    
+    projectsHtml += `
+                    </span>
+                </div>
+
+                <!-- footer -->
+                <footer>
+                    <a target="_blank" href="${project.demo}" class="lDemo">Live demo</a>
+                    <a target="_blank" href="" class="cStudy">Case study</a>
+                    <a target="_blank" href="${project.github}" class="github">Github</a>
+                </footer>
+            </article>
+        </div>
+    `
+}
+
+document.querySelector("#projects").innerHTML = projectsHtml;
