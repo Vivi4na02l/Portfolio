761df87d2065183b0b0439502edd377d9ff8c731
Viviana <40200339@esmad.ipp.pt>
2026-01-31 14:13:16 +0000

    footer complete

---
+++ JS/page.js
@@ -0,0 +1,25 @@
+getDate();
+startTime();
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    m = checkDigits(m);
+    document.querySelector('#digitalClock').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+}
+
+function checkDigits(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function getDate() {
+    const today = new Date();
+    let d = today.getDate();
+    let m = today.getMonth()+1;
+    m = checkDigits(m);
+    let y = today.getFullYear();
+    document.querySelector('#todaysDate').innerHTML = d + '/' + m + '/' + y;
+}
